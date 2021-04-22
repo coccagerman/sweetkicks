073c7488a71daf53af6805fdfa26f9f07e002d7e
Germán <coccagerman@gmail.com>
2021-04-21 22:46:23 -0300

    Improved dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,10 @@ import HeaderLogoDark from '../assets/sweetkicks_logo_header_dark.png'
 import HeaderLogoLight from '../assets/sweetkicks_logo_header_light.png'
 import WishlistIcon from './WishlistIcon'
 import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import arrowDownAlt2 from '@iconify-icons/dashicons/arrow-down-alt2';
 
 function Header ({setDarkMode, darkMode, wishListcounter}) {
-
-    function handleSwitchClick () {
-        setDarkMode(!darkMode)
-    }
 
     // Hook used to show and hide the dropdown menu
     const [showDropdown, setshowDropdown] = useState(false)
@@ -18,7 +16,9 @@ function Header ({setDarkMode, darkMode, wishListcounter}) {
             <img className='headerLogo' src={darkMode === false ? HeaderLogoLight : HeaderLogoDark} alt='Company logo' />
             <nav>
                 <ul>
-                    <li><a href="#" onClick={(e) => setshowDropdown(!showDropdown)}>Categories</a>
+                    <li><a href="#" onClick={() => setshowDropdown(!showDropdown)} className='categories'>Categories 
+                            <Icon icon={arrowDownAlt2} className={showDropdown === false ? 'arrowIcon' : 'arrowIcon open' }/> 
+                        </a>
                         <ul className={showDropdown === true ? 'dropdown' : 'hiddenDropdown' }>
                             <li><a href="#">Street</a></li>
                             <li><a href="#">Running</a></li>
@@ -35,7 +35,7 @@ function Header ({setDarkMode, darkMode, wishListcounter}) {
                     <li className='switch-container'>
                         <span className="iconify" data-icon="ion:sunny-outline" data-inline="false"></span>
                         <label className="switch" >
-                            <input type="checkbox" onClick={handleSwitchClick} />
+                            <input type="checkbox" onClick={() => setDarkMode(!darkMode)} />
                             <span className="slider round" />
                         </label>
                         <span className="iconify" data-icon="ion:moon-outline" data-inline="false"></span>
