7a04c2f0b072142b7acf92e301c37eaea7ac607c
Germán <coccagerman@gmail.com>
2021-04-26 21:14:35 -0300

    Added brand icon to each product card
    Added brand icon to each product card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,10 @@
-import Foto from '../assets/products/adidas/adidas1/adidas1_1.jpeg'
+import NikeIcon from '../assets/icons/nike-icon.png'
+import AdidasIcon from '../assets/icons/adidas-icon.png'
+import JordanIcon from '../assets/icons/jordan-icon.png'
+import PumaIcon from '../assets/icons/puma-icon.png'
+import ReebokIcon from '../assets/icons/reebok-icon.png'
+import UnderArmourIcon from '../assets/icons/underArmour-icon.png'
+
 import { useState } from 'react';
 import { Icon } from '@iconify/react';
 import heartSolid from '@iconify-icons/clarity/heart-solid';
@@ -19,12 +25,23 @@ function Item ({brand, model, color, price, stock, imgUrl, wishListcounter, setw
         wishedItem === false ? setwishListcounter(wishListcounter+1) : setwishListcounter(wishListcounter-1)
     }
 
+    // Function used to show the corresponding brand icon in each card
+    function braindIcon () {
+        if (brand === 'Nike') { return NikeIcon}
+        else if (brand === 'Adidas') { return AdidasIcon}
+        else if (brand === 'Jordan') { return JordanIcon}
+        else if (brand === 'Puma') { return PumaIcon}
+        else if (brand === 'Reebok') { return ReebokIcon}
+        else if (brand === 'Under Armour') { return UnderArmourIcon}
+    }
+
     return (
         <article class="item-card">
-            <img src={imgUrl} class="card-img" alt="Product"/>
-            <div class="card-body">
-                <h3 class="card-title">{model}</h3>
-                <p class="card-price">${numberThousandSeparator(price)}</p>
+            <img src={braindIcon()} className='brand-icon' alt="Brand icon"/>
+            <img src={imgUrl} className="card-img" alt="Product"/>
+            <div className="card-body">
+                <h3 className="card-title">{model}</h3>
+                <p className="card-price">${numberThousandSeparator(price)}</p>
                 <Icon icon={heartSolid} className={wishedItem === false ? 'notWished' : 'wished'} onClick={() => handleWishClick()}/>
             </div>
         </article>
