1f2a8f3589b08afc88bb40a58b43e7057a4ef73a
Germán <coccagerman@gmail.com>
2021-05-17 18:21:05 -0300

    Added animated buttons in hero
    Added animated buttons in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,22 +6,39 @@ function Hero ({darkMode, productsDataBase, setProductsArray}) {
     return (
         <section className='hero'>
             <img src={darkMode === false ? ImageLight : ImageDark} className='hero-img' alt="Shoe background"/>
-            <Link to='/gallery' onClick={() => setProductsArray(productsDataBase)}>
-                <div class="container">
-                    <a href="#" class="btn">
-                        <svg width="277" height="62">
-                            <defs>
-                                <linearGradient id="grad1">
-                                    <stop offset="0%" stop-color={darkMode ? '#ccc' : "#e03f31"}  />
-                                    <stop offset="100%" stop-color={darkMode ? '#ccc' : "#c4372b"} />
-                                </linearGradient>
-                            </defs>
-                            <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="266" height="50"></rect>
-                        </svg>
-                        <span>SHOW ME THE SHOES</span>
-                    </a>
+
+
+
+
+                <div class="cta-container">
+
+                    <a class="container-arrow arrow-left scroll-to" href="#cards"><span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
+
+                    <Link to='/gallery' onClick={() => setProductsArray(productsDataBase)}>
+                        <a href="#" class="btn">
+                            <svg width="277" height="62">
+                                <defs>
+                                    <linearGradient id="grad1">
+                                        <stop offset="0%" stop-color={darkMode ? '#ccc' : "#e03f31"}  />
+                                        <stop offset="100%" stop-color={darkMode ? '#ccc' : "#c4372b"} />
+                                    </linearGradient>
+                                </defs>
+                                <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="266" height="50"></rect>
+                            </svg>
+                            <span>SHOW ME THE SHOES</span>
+                        </a>
+                    </Link>
+
+                    <a class="container-arrow arrow-right scroll-to" href="#cards"><span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
+
                 </div>
-            </Link>
+
+
+            <div class="arrow-right">
+                <div class="left"></div>
+                <div class="right"></div>
+            </div>
+
         </section>
     )
 }
