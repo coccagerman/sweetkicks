3a7f66b602be66d8e398c84580173923cc515ab5
Germán <coccagerman@gmail.com>
2021-07-24 13:39:12 -0300

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import FooterLogo from '../../assets/sweetkicks_logo_footer.png'
 
-function Footer ({fixFooter}) {
+function Footer () {
     return (
         <>
         <footer>
             <Link to='/'><img className='footerLogo' src={FooterLogo} alt='Company logo' /></Link>
             <div className='socialNetwork-container'>
-                <a href="https://www.facebook.com/" target="_blank"><i class="socialNetwork fab fa-facebook-square"></i></a>
-                <a href="https://www.twitter.com/" target="_blank"><i class="socialNetwork fab fa-twitter"></i></a>
-                <a href="https://www.instagram.com/" target="_blank"><i class="socialNetwork fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i class="socialNetwork fab fa-facebook-square"></i></a>
+                <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><i class="socialNetwork fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i class="socialNetwork fab fa-instagram"></i></a>
             </div>
         </footer>   
         </>
